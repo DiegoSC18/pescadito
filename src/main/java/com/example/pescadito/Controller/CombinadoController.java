b5eee28a59cc0b112d2218a44eeb17af4755620c
DiegoSC18 <U23215658@utp.edu.pe>
2025-11-18 10:40:42 -0500

    V2

--- src/main/java/com/example/pescadito/Controller/CombinadoController.java
+++ src/main/java/com/example/pescadito/Controller/CombinadoController.java
@@ -1,4 +1,23 @@
 package com.example.pescadito.Controller;
 
+import com.example.pescadito.Model.Combinado;
+import com.example.pescadito.Service.CombinadoService;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("/api/combinado")
+
 public class CombinadoController {
+
+
 }
+
+
+
+
+
+
+
